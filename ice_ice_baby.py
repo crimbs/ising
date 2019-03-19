ede08c83c41ea5500f18043692e7c62d410338fb
Christian Hines <christianhines@Christians-MacBook-Pro.local>
2019-03-19 19:53:25 +0000

    did some more stuff

--- ice_ice_baby.py
+++ ice_ice_baby.py
@@ -3,31 +3,27 @@
 # ising.py
 import numpy as np
 
-np.random.seed(seed=1234)
+#np.random.seed(seed=1234)
 #alternative_random_gen = [-1,1][np.random.randint(2)]
 
 # Define global variables
-N = 16                  # N x N lattice sites
-T = 1                   # Temperature [K]
-k = 1.38064852e-23      # Boltzmann's Constant [m^2 kg s^-2 K^-1]
-n = 10**2               # Number of time steps
+N = 16                          # N x N lattice sites
+T = 0.1                           # Temperature [K]
+#k = 1.38064852e-23              # Boltzmann's Constant [m^2 kg s^-2 K^-1]
+num_steps = 10**3               # Number of time steps
 
 # Initialisation
-def lattice(N):
-    a = np.random.randint(2, size=(N,N))
-    return np.where(a==0, -1, a)
+lattice = np.random.choice([1, -1], size=(N, N))
 
-lattice = lattice(N)
+for step in range(num_steps):
+    
+    i = np.random.randint(N)
+    j = np.random.randint(N)
 
-for j in range(n):
-    for i in range(N):
-        for j in range(N):
-            E = lattice[((i - 1) % N), j] + \
-                lattice[((i + 1) % N), j] + \
-                lattice[i, ((j - 1) % N)] + \
-                lattice[i, ((j + 1) % N)]
-            if E < 0:
-                lattice[i, j] = -lattice[i, j]
-            else:
-                if np.exp(-E/(k*T)) > np.random.rand():
-                    lattice[i, j] = -lattice[i, j]
+    dE = lattice[i,j]*lattice[((i - 1) % N), j] + \
+        lattice[((i + 1) % N), j] + \
+        lattice[i, ((j - 1) % N)] + \
+        lattice[i, ((j + 1) % N)]
+        
+    if dE < 0 or np.exp(-dE/T) > np.random.rand():
+        lattice[i, j] = -lattice[i, j]
